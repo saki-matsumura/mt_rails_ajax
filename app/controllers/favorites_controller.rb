0f85266d7ededb357610919ddc03c5e3afeca49d
“MatsumuraSaki” <massaki3156@gmail.com>
2024-02-20 12:27:57 +0900

    Ajaxによるお気に入り登録機能実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,17 @@
 class FavoritesController < ApplicationController
 
   def create
-    current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
+    @favorite = current_user.favorites.create(blog_id: params[:blog_id])
+    respond_to do |format|
+      format.js { render 'blogs/favorite' }
+    end
   end
 
   def destroy
-    current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
+    @favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    respond_to do |format|
+      format.js { render 'blogs/favorite' }
+    end
   end
 
 end
